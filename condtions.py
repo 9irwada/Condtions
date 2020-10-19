d1575bcacc20fe45fed366f809e2bba74a983664
saint <saint@kali.tecmintlocal.com>
2020-10-20 02:06:33 +0300

    first commit

---
+++ condtions.py
@@ -0,0 +1,6 @@
+username  = input('Please insert your usernmae :')
+passowrd  = input('Please insert your passowrd :')
+if username == 'Nabiot' and passowrd == '2424':
+    print ('Hi Baby')
+else:
+    print ('Error')
